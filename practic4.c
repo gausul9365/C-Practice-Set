610f3a313dccffb764a6a152f2bdf9ec3bcc0728
gausul9365 <khurrram9365@gmail.com>
2024-09-25 12:02:20 +0530

    ccc

---
+++ practic4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+// Write a program to print natural numbers from 10 to 20 when initial loop counter is initialized to 0.
+
+// int main(){
+
+// int i= 10;
+
+// while (i<=20){
+
+//   printf("%d\n", i);
+//   i++;
+// }
+// return 0;
+// }
+
+
+// : Write a program to print first ‘n’ natural number using do-while loop.
+
+// int main() {
+//     int n;
+//     printf("Enter your number:");
+//     scanf("%d", &n);
+//     int i = 1;
+//     while (i <= n) {
+//         printf("%d\n", i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// continue
+
+//  #include <stdio.h>
+// int main() {
+// int skip = 5;
+// int i = 0;
+// while (i < 10) {
+// if (i == skip) {
+// i++;
+// continue; // skips the rest of the loop body for i == 5
+// }
+// printf("%d\n", i);
+// i++;
+// }
+// return 0;
+// }
+
